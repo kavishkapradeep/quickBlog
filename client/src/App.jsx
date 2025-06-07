3bdc0da6046b5ea0f963e85db23ace51eef75582
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2025-06-08 02:52:57 +0530

    blog newsletter setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className=' text-9xl'>
-        test
-    </div>
+    < >
+      <Navbar/>
+      <Routes>
+         <Route path='/' element={<Home/>}/>
+         <Route path='/blog/:id' element={<Blog/>}/>
+      </Routes>
+    </>
   );
 }
 
