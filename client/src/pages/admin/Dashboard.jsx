fdc7c089858f53bccce38b56b0ae988bd9f56359
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2025-06-10 01:48:01 +0530

    server start

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { assets, dashboard_data } from '../../assets/assets';
+import BlogTableItem from '../../components/admin/BlogTableItem';
 
 const Dashboard = () => {
   const [dashboardData,setDashboardData] = useState({
@@ -64,7 +65,10 @@ const Dashboard = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        
+                        {dashboardData.recentBlogs.map((blog,index)=>{
+                          return  <BlogTableItem key={blog._id} blog={blog}
+                          fetchBlogs={fetchDashboard} index={index+1}/>
+                        })}
                     </tbody>
                 </table>
             </div>
