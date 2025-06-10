9d81a0245fa03daa383aa6fe1ba58df4fb1616fa
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2025-06-11 01:54:04 +0530

    remove conso;e ;og

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -6,8 +6,6 @@ export const adminLogin = async (req,res) => {
     try {
         const {email,password} = req.body;
 
-        console.log(process.env.ADMIN_EMAIL);
-        
         if (email !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD) {
             return res.json({success:false,message:"Invalid credentials"})
         }
